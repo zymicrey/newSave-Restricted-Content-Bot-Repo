2c61b7f80fe7c8cfaaae285d22dee70393964599
zymicrey <162611695+zymicrey@users.noreply.github.com>
2024-03-08 17:10:59 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -1,4 +1,4 @@
-#Join me @dev_gagan
+#Join me @Remo
 
 import logging
 import time
@@ -18,7 +18,7 @@ botStartTime = time.time()
 
 
 print("Successfully deployed!")
-print("Bot Deployed : Team SPY")
+print("Bot Deployed : Remo")
 
 if __name__ == "__main__":
     from . import bot
